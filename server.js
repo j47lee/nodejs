b838bc327cf30573ae3a777246821026747a62ef
Jonathan Lee <j47lee@gmail.com>
2015-09-22 07:08:29 -0700

    removed node modules

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ var profile = require("./profile");
 
 var users = process.argv.slice(2);
 users.forEach(profile.getProfile);
+
+//run this command in the terminal to request josh timeonen profile from teamtreehouse
+//node server.js joshtimonen
